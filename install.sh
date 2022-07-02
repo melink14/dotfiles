fd520f17af17368fadb2e8844a82abd6ed460d4f
Erek Speed <melink14@gmail.com>
2022-07-02 20:13:46 +0900

    fix: Properly run installation scripts in non-interactive mode

--- install.sh
+++ install.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+# Install a nerd font needed by starship
+echo "[-] Download fonts [-]"
+echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CascadiaCode.zip"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CascadiaCode.zip
+unzip CascadiaCode.zip -d ~/.fonts
+fc-cache -fv
+echo "done!"
+
 sudo apt update
 DEBIAN_FRONTEND=noninteractive \
   sudo apt \
   -y install rcm fzf
-sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+
+
+curl -sS https://starship.rs/install.sh | sh -s -- --yes
 
 git clone https://github.com/4z3/fzf-plugins ~/.fzf-plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-RCRC=./rcrc rcup -d .
+RCRC=./rcrc rcup -f -d .
 
 echo "Installation complete. Install tmux plugins with <prefix>-I"
